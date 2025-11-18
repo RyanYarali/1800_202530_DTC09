c411440f371b0262285c8aca67be8beddea71864
Brendan Jang <brendanjang2007@gmail.com>
2025-11-18 11:59:08 -0800

    Change "groups" icon

--- src/components/site-bottom-bar.js
+++ src/components/site-bottom-bar.js
@@ -1,6 +1,6 @@
 class SiteBottomBar extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
+	connectedCallback() {
+		this.innerHTML = `
 			<!-- Footer: single source of truth -->
 			<link rel="stylesheet" href="./src/styles/style.css" />
 			<footer
@@ -62,31 +62,38 @@ class SiteBottomBar extends HTMLElement {
 				</a>
 								<a
 				href="groups.html"
-				class="flex flex-col items-center bg-white shadow-lg rounded-2xl px-4 py-2 "
+				class="flex flex-col items-center px-4 py-2 "
 				style="color: #230007;"
 				onmouseover="this.style.color='#A40606'"
 				onmouseout="this.style.color='#230007'"
 				>
-
-								<svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="32"
-    height="32"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="#230007"
-    stroke-width="1"
-    stroke-linecap="round"
-    stroke-linejoin="round"
->
-
-
-    <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
-    <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2" />
-    <circle cx="12" cy="14" r="3" />
-    <path d="M12 12v2l1.5 1.5" />
-</svg>
-
+				<svg 
+					width="32"
+					height="32" viewBox="0 0 24 24"
+					fill="none" xmlns="http://www.w3.org/2000/svg"
+					stroke="#230007">
+					<g 	
+						id="SVGRepo_bgCarrier" 
+						stroke-width="0">
+					</g>
+					<g 
+						id="SVGRepo_tracerCarrier"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke="#CCCCCC"
+						stroke-width="0.096">
+					</g>
+					<g
+						id="SVGRepo_iconCarrier">
+						<path 
+							d="M13 20V18C13 15.2386 10.7614 13 8 13C5.23858 13 3 15.2386 3 18V20H13ZM13 20H21V19C21 16.0545 18.7614 14 16 14C14.5867 14 13.3103 14.6255 12.4009 15.6311M11 7C11 8.65685 9.65685 10 8 10C6.34315 10 5 8.65685 5 7C5 5.34315 6.34315 4 8 4C9.65685 4 11 5.34315 11 7ZM18 9C18 10.1046 17.1046 11 16 11C14.8954 11 14 10.1046 14 9C14 7.89543 14.8954 7 16 7C17.1046 7 18 7.89543 18 9Z" 
+							stroke="#230007" 
+							stroke-width="1.2" 
+							stroke-linecap="round" 
+							stroke-linejoin="round">
+						</path>
+					</g>
+				</svg>
 				<span class="text-sm">Groups</span>
 				</a>
 								<a
@@ -148,7 +155,7 @@ class SiteBottomBar extends HTMLElement {
 			</div>
 			</footer>
 		`;
-  }
+	}
 }
 
 customElements.define("site-bottom-bar", SiteBottomBar);
